81153283b7871a61ab7f4b4f0d61b60569d1f188
Daniil Pakholkov <daniil.paholkov@yandex.ru>
2024-04-11 23:27:48 +0300

    Create add card function and delete card function

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,22 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const cardsContainer = document.querySelector('.places__list');
+function deleteCard(event) {
+  event.target.parentElement.remove();
+}
+
+function createCard(cardName, cardImgLink, deleteCardFunction) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  cardElement.querySelector('.card__title').textContent = cardName;
+  cardElement.querySelector('.card__image').src = cardImgLink;
+  cardDeleteButton.addEventListener('click', deleteCardFunction);
+  cardsContainer.append(cardElement);
+}
+
+for(let i = 0; i<initialCards.length; i++) {
+  createCard(initialCards[i].name, initialCards[i].link, deleteCard);
+}
+
